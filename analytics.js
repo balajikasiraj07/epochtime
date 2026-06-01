248187a883fa4f7e2707f2c87358b8456f86e51c
Balaji Kasiraju <kasiraju.balaji@gmail.com>
2026-06-01 22:10:00 +0530

    Replace GA with GTM (GTM-54Z873L3) across all pages

--- analytics.js
+++ analytics.js
@@ -1,10 +1,8 @@
-// Central analytics & ads — edit this file to update tracking across all pages
+// Central analytics — edit this file to update tracking across all pages
 
-// Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-9ZKKD47Q2K');
-
-// Google AdSense auto-ads
-(adsbygoogle = window.adsbygoogle || []).push({});
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-54Z873L3');
